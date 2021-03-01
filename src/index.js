818d294ecd0dddfea80d75a0e40c834bf7c98c4a
“Aaron <github email address>
2021-03-01 12:04:28 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const toyCollection = document.querySelector("div#toy-collection")
+const toyForm = document.querySelector("form.add-toy-form")
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,3 +15,76 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+fetch ("http://localhost:3000/toys", {
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+})
+  .then(resp => resp.json())
+  .then(toyList => {
+    toyList.forEach(toyObj => {
+      renderOneToy(toyObj)
+    })
+  })
+
+function renderOneToy(toyObj) {
+  const div =  document.createElement('div')
+  div.classList.add('card')
+  div.dataset.id = toyObj.id
+
+  div.innerHTML = `
+  <h2>${toyObj.name}</h2>
+  <img src=${toyObj.image} class="toy-avatar" />
+  <p>${toyObj.likes} likes </p>
+  <button class="like-btn">Like <3</button>
+  `
+  toyCollection.append(div)
+}
+
+toyForm.addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  const name = event.target.name.value
+  const image = event.target.image.value
+
+  const toyObj = {
+    name: name,
+    image: image,
+    likes: 0
+  }
+
+
+  fetch("http://localhost:3000/toys", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(toyObj),
+  })
+    .then(resp => resp.json())
+    .then(renderOneToy(toyObj))
+  
+  toyForm.reset()
+
+})
+
+toyCollection.addEventListener('click', function (event) {
+  if (event.target.matches('button.like-btn')) {
+    const currentLikes = event.target.parentNode.querySelector("p")
+    const newLikes = parseInt(currentLikes.textContent) + 1
+  fetch(`http://localhost:3000/toys/${event.target.parentNode.dataset.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      likes: newLikes
+    })
+  })
+  .then(resp => resp.json())
+  .then(data => {currentLikes.textContent = `${newLikes} likes`)}
+})
